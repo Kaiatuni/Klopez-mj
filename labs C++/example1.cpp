ff83ab584df1c26121a1dc0b1ff2f3cfd8b0b116
Kivan Lopez <kivanmiguel2206@gmail.com>
2024-03-07 09:42:55 -0400

    update file

--- labs C++/example1.cpp
+++ labs C++/example1.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main()
 {
-    int myVariable
+    int myVariable = 10; 
+    const int myConstant = 20;
 
-    
+    cout <<"Valor de mi variable: " << myVariable << endl;
+    cout <<"Valor de mi Constante:" << myConstant << endl;
+
+    return 0;
 }
--- labs JAVA/example1/example1/src/App.java
+++ labs JAVA/example1/example1/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int myVariable = 10;
+        final int myConstant = 20;
+
+        system.out.println("Valor de mi variable:" + myVariable);
+        system.out.println("")
     }
 }
