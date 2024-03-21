b0c7a27c0f4aaa74b037f0a1a99b0685917274dc
kivan Lopez <kivanmiguel2206@gmail.com>
2024-03-21 10:52:24 -0300

    update file

--- labs C++/ejercicio3.cpp
+++ labs C++/ejercicio3.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 int main()
 {
-    int yearact, yearborn, edadaprox ;
+    int yearact, yearborn, edadaprox ; // variables necesarias para calcular la edad aprox //
 
     cout << "Ingrese el year actual";
     cin >> yearact;
 
     cout <<"Ingrese el year en que nacio";
-    cin >> 
+    cin >> yearborn;
+
+    edadaprox = yearact - yearborn; // proceso para calcular //
+
+    cout <<"La edad aproximada es de: "<<edadaprox<< endl;
+
+    return 0;
 }
