466369e1ba44899309d46d436edaf0af9df2a402
kivan Lopez <kivanmiguel2206@gmail.com>
2024-03-21 11:33:11 -0300

    update file

--- labs C++/ejercicio5.cpp
+++ labs C++/ejercicio5.cpp
@@ -5,5 +5,16 @@ int main ()
 {
     int area, Base , altura;
 
-    cout << "Ingrese la altura del "
+    cout << "Ingrese la altura del retangulo: ";
+    cin >> altura;
+
+    cout << "Ingrese la base del rectangulo: "; // Aqui se ingresa la base del rectangulo //
+    cin >> Base;
+
+    area = Base * altura;
+
+    cout <<"El area del rectangulo es: " <<area<< endl; // Printea el area del rectangulo //
+
+    return 0;
+
 }
