0d9d46f58ca6cedda932a51e3b344ac784c650ea
kivan Lopez <kivanmiguel2206@gmail.com>
2024-03-21 11:05:24 -0300

    update file

---
+++ labs C++/ejercicio4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int area, lado; //Variables necesarias para este procedimiento //
+
+    cout <<"Ingrese el lado de el cuadrado: ";
+    cin >> lado;
+
+    area = lado * lado;
+
+    cout <<"El area de el cuadrado es: "<<area<< endl;
+
+    return 0;
+    
+}
---
+++ labs C++/ejercicio5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int area, Base , altura;
+
+    cout << "Ingrese la altura del "
+}
