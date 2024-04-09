e0263f90a475676a5a61012b001e2646535c4621
kivan Lopez <kivanmiguel2206@gmail.com>
2024-04-09 13:05:10 -0400

    update file

---
+++ labs C++/estructcomple.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float total_compras; // definiendo variables //
+    char miembro_vip;
+    //solicita al usuario que ingrese el total de compras//
+    cout << "Ingrese el total de compras: ";
+    cin >> total_compras;
+
+    cout << "Es miembro de VIP (s/n): ";
+    cin >> miembro_vip;
+
+    miembro_vip = tolower(miembro_vip); 
+// El if se usa para preguntar si el usuario tiene vip o no //
+    if(total_compras >= 150 || miembro_vip == 's') { 
+        cout << 'Desuento aplicado' <<endl;
+    } else {
+        cout << "SIn descuento aplicado" <<endl;
+    }
+    return 0;
+}
--- labs C++/nuevasuma.cpp
+++ labs C++/nuevasuma.cpp
@@ -10,16 +10,5 @@ int main ()
 
     cout <<"Ingrese la cantidad de estudiantes: " <<endl;
     cin >> cantidadEstudiante;
-
-    if(cantidadEstudiante> 10)
-    {
-      
-        cout << "La cantidad de estudiantes es mayor de 10, ingrese una cantidad menor de diez." <<endl;
-
-    }
-
-    else
-
-
-
+    
 }
