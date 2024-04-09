14916e79318e8c83eac0db2a29471e1de76a38ba
kivan Lopez <kivanmiguel2206@gmail.com>
2024-04-09 10:05:40 -0400

    update fiel

---
+++ labs C++/nuevasuma.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std;
+
+int main ()
+{
+    int sumaClasificaciones = 0;
+    int promedio, calificacionEstudiante;
+    string numeroEstudiante;
+    int cantidadEstudiante;
+
+    cout <<"Ingrese la cantidad de estudiantes: " <<endl;
+    cin >> cantidadEstudiante;
+
+    if(cantidadEstudiante> 10)
+    {
+      
+        cout << "La cantidad de estudiantes es mayor de 10, ingrese una cantidad menor de diez." <<endl;
+
+    }
+
+    else
+
+
+
+}
