4d6c03730c597834e3b4d1a6b0863c06223f4f84
kivan Lopez <kivanmiguel2206@gmail.com>
2024-04-11 09:58:40 -0400

    update file

---
+++ labs JAVA/forandwhile/forwhile/src/App.java
@@ -0,0 +1,10 @@
+public class forwhile {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        int nombre;
+
+        System.out.println ("Ingrese su nombre");
+        
+
+    }
+}
